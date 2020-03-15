6cc800c248c3ebe87e0172daf2246a094f594c1e
bangswer <62163934+bangswer@users.noreply.github.com>
2020-03-15 21:46:45 +0900

    Add files via upload

---
+++ gamer's-bot.py
@@ -0,0 +1,156 @@
+import discord
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+      print(client.user.id)
+      print("ready")
+      game = discord.Game("빵숴님을 존경")
+      await client.change_presence(status=discord.Status.idle, activity=game)
+
+
+
+@client.event
+
+    if message.content.startswith("ㅅㅂ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅄ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅂㅅ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("개새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("시발"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("병신"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("븅신"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("쉬벌"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("시벌"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("쉬발"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("좃까"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("조까"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("육시럴"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("씹새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("좃같은 새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("좃같은새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("병신새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("병신 새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("븅신새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("븅신 새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("시발새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("시발 새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("시벌새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("시벌 새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("쉬발새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("쉬벌 새끼"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗㅗㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗㅗㅗㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗㅗㅗㅗㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗㅗㅗㅗㅗㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗㅗㅗㅗㅗㅗㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗㅗㅗㅗㅗㅗㅗㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗㅗㅗㅗㅗㅗㅗㅗㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+
+    if message.content.startswith("애미"):
+        role = discord.utils.get(message.guild.roles, name="blacklist")
+        await message.author.add_roles(role)
+
+    if message.content.startswith("애미 없는 놈"):
+        role = discord.utils.get(message.guild.roles, name="blacklist")
+        await message.author.add_roles(role)
+
+    if message.content.startswith("애미 없는 년"):
+        role = discord.utils.get(message.guild.roles, name="blacklist")
+        await message.author.add_roles(role)
+
+    if message.content.startswith("애미 없는 년아"):
+        role = discord.utils.get(message.guild.roles, name="blacklist")
+        await message.author.add_roles(role)
+
+    if message.content.startswith("애미 없는 놈아"):
+        role = discord.utils.get(message.guild.roles, name="blacklist")
+        await message.author.add_roles(role)
+
+    if message.content.startswith("애미 없는 놈아"):
+        role = discord.utils.get(message.guild.roles, name="blacklist")
+        await message.author.add_roles(role)
+
+    if message.content.startswith("호날두"):
+        await message.channel.send(message.author.name + "님 욕설로 경고1회")
+        await message.author.add_roles(role)
+
+
+
+
+client.run("Njg0MjY3MDk0NzQ0ODkxNDU3.Xl3n8Q.Pqz-w8cwJndxZaxd9GwrnA2RbsI")
